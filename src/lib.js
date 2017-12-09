839c1be7259860395e05fbf0084aa0a7e88f813b
Fabio Ricali <ricalifabio@gmail.com>
2017-12-09 11:49:57 +0100

    refactoring

--- src/lib.js
+++ src/lib.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const extend = require('defaulty');
 const URL = require('url');
 const queryString = require('querystring');
-const dataUtils = require('./dataUtils');
+const dataUtils = require('./data-utils');
 
 class StaticBuster{
 
